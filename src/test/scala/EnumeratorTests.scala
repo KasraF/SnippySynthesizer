1cbac60f2a00927a801012f4e9353ac9f1a5290c
peleghila <peleg.hila@gmail.com>
2019-11-27 18:40:59 -0800

    seeing enumerator change level

--- src/test/scala/EnumeratorTests.scala
+++ src/test/scala/EnumeratorTests.scala
@@ -12,7 +12,6 @@ class EnumeratorTests  extends JUnitSuite{
         #Literal|0|1
         #BinOperator|2|+
         #BinOperator|2|<=
-        #BinOperator|2|!=
         #FunctionCall|1|str""".stripMargin('#')
     )
     val enumerator = new Enumerator(vocab)
@@ -30,5 +29,52 @@ class EnumeratorTests  extends JUnitSuite{
     assertEquals("input + 0",enumerator.next().code)
     assertEquals("input + 1",enumerator.next().code)
     assertTrue(enumerator.hasNext)
+    assertEquals("False + input",enumerator.next().code)
+    assertEquals("False + False",enumerator.next().code)
+    assertEquals("False + 0",enumerator.next().code)
+    assertEquals("False + 1",enumerator.next().code)
+    assertTrue(enumerator.hasNext)
+    assertEquals("0 + input",enumerator.next().code)
+    assertEquals("0 + False",enumerator.next().code)
+    assertEquals("0 + 0",enumerator.next().code)
+    assertEquals("0 + 1",enumerator.next().code)
+    assertTrue(enumerator.hasNext)
+    assertEquals("1 + input",enumerator.next().code)
+    assertEquals("1 + False",enumerator.next().code)
+    assertEquals("1 + 0",enumerator.next().code)
+    assertEquals("1 + 1",enumerator.next().code)
+    assertTrue(enumerator.hasNext)
+    assertEquals("input <= input",enumerator.next().code)
+    assertEquals("input <= False",enumerator.next().code)
+    assertEquals("input <= 0",enumerator.next().code)
+    assertEquals("input <= 1",enumerator.next().code)
+    assertTrue(enumerator.hasNext)
+    assertEquals("False <= input",enumerator.next().code)
+    assertEquals("False <= False",enumerator.next().code)
+    assertEquals("False <= 0",enumerator.next().code)
+    assertEquals("False <= 1",enumerator.next().code)
+    assertTrue(enumerator.hasNext)
+    assertEquals("0 <= input",enumerator.next().code)
+    assertEquals("0 <= False",enumerator.next().code)
+    assertEquals("0 <= 0",enumerator.next().code)
+    assertEquals("0 <= 1",enumerator.next().code)
+    assertTrue(enumerator.hasNext)
+    assertEquals("1 <= input",enumerator.next().code)
+    assertEquals("1 <= False",enumerator.next().code)
+    assertEquals("1 <= 0",enumerator.next().code)
+    assertEquals("1 <= 1",enumerator.next().code)
+    assertTrue(enumerator.hasNext)
+    assertEquals("str(input)",enumerator.next().code)
+    assertEquals("str(False)",enumerator.next().code)
+    assertEquals("str(0)",enumerator.next().code)
+    assertEquals("str(1)",enumerator.next().code)
+    assertTrue(enumerator.hasNext)
+
+    assertEquals("input + (input + input)", enumerator.next().code)
+    assertTrue(enumerator.hasNext)
+  }
+
+  @Test def enumerateVocabWithOE: Unit = {
+    
   }
 }
