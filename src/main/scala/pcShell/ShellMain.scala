bcf074889059236170fa7f6245b35cd2cea40f23
peleghila <peleg.hila@gmail.com>
2020-01-01 17:33:09 -0800

    pretty errors

--- src/main/scala/pcShell/ShellMain.scala
+++ src/main/scala/pcShell/ShellMain.scala
@@ -12,12 +12,45 @@ import trace.DebugPrints.iprintln
 object ShellMain extends App {
   val taskFilename = "src/test/benchmarks/too-hard/39060015.sl"//args(0)
   val task = new SygusFileTask(scala.io.Source.fromFile(taskFilename).mkString)
+
+  def escapeWSAndQuote(s: String) = { //		if ( s==null ) return s;
+    "'" + s.replace("\n", "\\n")
+     .replace("\r", "\\r")
+     .replace("\t", "\\t") + "'"
+  }
+
+  def getTokenErrorDisplay(t: Token): String = {
+    if (t == null) return "<no token>"
+    var s = t.getText
+    if (s == null) if (t.getType == Token.EOF) s = "<EOF>"
+    else s = "<" + t.getType + ">"
+    escapeWSAndQuote(s)
+  }
+  def prettyPrintSyntaxError(exception: RecognitionException) = {
+    val offset = if (exception.isInstanceOf[LexerNoViableAltException]) exception.asInstanceOf[LexerNoViableAltException].getStartIndex + 2 else exception.getOffendingToken.getStartIndex + 1
+    print(" " * (offset) + "^")
+    if (exception.getOffendingToken != null && exception.getOffendingToken.getStopIndex - exception.getOffendingToken.getStartIndex > 1)
+      print("-" * (exception.getOffendingToken.getStopIndex - exception.getOffendingToken.getStartIndex - 1) + "^")
+    println
+    exception match {
+      case e: NoViableAltException =>
+        println("no viable alternative at input " + (if (e.getStartToken == Token.EOF) "<EOF>" else escapeWSAndQuote(e.getOffendingToken.getText)))
+      case e: InputMismatchException =>
+        println( s"mismatched input ${getTokenErrorDisplay(e.getOffendingToken())}, expecting ${e.getExpectedTokens().toString()}")
+      case e: LexerNoViableAltException =>
+        println("bad token")
+    }
+  }
+
   Iterator.continually{
     print("> ")
     Console.in.readLine()
   }.takeWhile(_ != null).foreach { line =>
       if (!line.trim.isEmpty) try {
-      val parser = new SyGuSParser(new BufferedTokenStream(new SyGuSLexer(CharStreams.fromString(line))))
+      val lexer = new SyGuSLexer(CharStreams.fromString(line))
+      lexer.removeErrorListeners()
+      lexer.addErrorListener(new ThrowingLexerErrorListener)
+      val parser = new SyGuSParser(new BufferedTokenStream(lexer))
       parser.removeErrorListeners()
       parser.setErrorHandler(new BailErrorStrategy)
       val parsed = parser.bfTerm()
@@ -26,13 +59,13 @@ object ShellMain extends App {
       Some(ast, task.examples.map(_.output))
     } catch {
       case e: RecognitionException => {
-        println(s"Cannot parse program: ${e.getMessage}")
+        prettyPrintSyntaxError(e)
       }
       case e: ResolutionException => {
         println(s"Cannot resolve program: ${e.msg}")
       }
       case e: ParseCancellationException =>{
-        println(s"Cannot parse program: ${e.getCause.asInstanceOf[RecognitionException]}")
+        prettyPrintSyntaxError(e.getCause.asInstanceOf[RecognitionException])
       }
     }
   }
