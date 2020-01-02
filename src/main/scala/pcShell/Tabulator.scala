3ba3d688bfa514c94823a380785c5cb31dc68e35
peleghila <peleg.hila@gmail.com>
2020-01-01 22:00:47 -0800

    newlines in table

--- src/main/scala/pcShell/Tabulator.scala
+++ src/main/scala/pcShell/Tabulator.scala
@@ -4,8 +4,10 @@ object Tabulator {
   def format(table: Seq[Seq[Any]]) = table match {
     case Seq() => ""
     case _ =>
-      val sizes = for (row <- table) yield (for (cell <- row) yield if (cell == null) 0 else cell.toString.length)
-      val colSizes = for (col <- sizes.transpose) yield col.max
+      val sizes = for (row <- table) yield (for (cell <- row) yield
+        if (cell == null) 0
+        else cell.toString.split('\n').map(_.length).max)
+      val colSizes = for (col <- sizes.transpose) yield col.max + 2
       val rows = for (row <- table) yield formatRow(row, colSizes)
       formatRows(rowSeparator(colSizes), headerRowSeparator(colSizes), rows)
   }
@@ -18,8 +20,13 @@ object Tabulator {
       List()).mkString("\n")
 
   def formatRow(row: Seq[Any], colSizes: Seq[Int]) = {
-    val cells = (for ((item, size) <- row.zip(colSizes)) yield if (size == 0) "" else /*("%" + size + "s").format(item)*/item.toString.padTo(size,' '))
-    cells.mkString("|", "|", "|")
+    val lines = row.map(item => item.toString.split('\n'))
+    val innerRows = for(rowNum <- 0 until lines.map(_.length).max) yield {
+      val innerRow = lines.map(l => if (rowNum < l.length) l(rowNum) else "")
+      val cells = (for ((item, size) <- innerRow.zip(colSizes)) yield if (size == 0) "" else /*("%" + size + "s").format(item)*/ " " + item.toString.padTo(size - 1, ' '))
+      cells.mkString("|", "|", "|")
+    }
+    innerRows.mkString("\n")
   }
 
   def headerRowSeparator(colSizes: Seq[Int]) = colSizes map { "=" * _ } mkString("+", "+", "+")
