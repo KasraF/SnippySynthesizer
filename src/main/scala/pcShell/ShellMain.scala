449e99730c58c040d97d5869753207c924a8acec
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2020-01-03 21:32:13 -0800

    Output number of examples satisfied; colon-number refers to recent result

--- src/main/scala/pcShell/ShellMain.scala
+++ src/main/scala/pcShell/ShellMain.scala
@@ -6,11 +6,14 @@ import org.antlr.v4.runtime.misc.ParseCancellationException
 import org.antlr.v4.runtime._
 import pcShell.Tabulator.GreenString
 import sygus._
+import scala.util.control.Exception.allCatch
+
 
 
 object ShellMain extends App {
   val taskFilename = args(0)
   val task = new SygusFileTask(scala.io.Source.fromFile(taskFilename).mkString)
+  var currentResults: List[String] = Nil
 
   val errorColor = Console.RED
   val infoColor = Console.YELLOW
@@ -54,6 +57,39 @@ object ShellMain extends App {
 
   def escapeIfString(elem: Any): String = if (elem.isInstanceOf[String]) escapeWSAndQuote(elem.asInstanceOf[String]) else elem.toString
 
+  def evalExpr(s: String) = try {
+    val lexer = new SyGuSLexer(CharStreams.fromString(s))
+    lexer.removeErrorListeners()
+    lexer.addErrorListener(new ThrowingLexerErrorListener)
+    val parser = new SyGuSParser(new BufferedTokenStream(lexer))
+    parser.removeErrorListeners()
+    parser.setErrorHandler(new BailErrorStrategy)
+    val parsed = parser.bfTerm()
+    if (parser.getCurrentToken.getType != Token.EOF)
+      throw new EOFExpectedException(parser)
+    val visitor = new ASTGenerator(task)
+    val ast = visitor.visit(parsed)
+    out.println(Tabulator.format(List("input","result","expected") +:
+      task.examples.zip(ast.values).map(pair => List(pair._1.input.toList.map(kv =>
+        s"${kv._1} -> ${escapeIfString(kv._2.toString)}"
+      ).mkString("\n"),
+        if (pair._2 == pair._1.output) GreenString(escapeIfString(pair._2)) else escapeIfString(pair._2),
+        escapeIfString(pair._1.output)))))
+  } catch {
+    case e: RecognitionException => {
+      prettyPrintSyntaxError(e)
+    }
+    case e: ResolutionException => {
+      val startIdx = e.badCtx.getStart.getStartIndex
+      val len = e.badCtx.getStop.getStopIndex - startIdx + 1
+      out.println(errorColor + " " * (startIdx + 2) + "^" + (if (len > 1) "-" * (len - 2) + "^" else ""))
+      out.println("Cannot resolve program" + Console.RESET)
+    }
+    case e: ParseCancellationException =>{
+      prettyPrintSyntaxError(e.getCause.asInstanceOf[RecognitionException])
+    }
+  }
+
 //  import jline.TerminalFactory
 //  jline.TerminalFactory.registerFlavor(TerminalFactory.Flavor.WINDOWS, classOf[UnsupportedTerminal])
   val reader = new ConsoleReader()
@@ -64,8 +100,9 @@ object ShellMain extends App {
 
   var line: String = null
   while ((line = reader.readLine()) != null) {
-      if (!line.trim.isEmpty) try {
-      if (line.trim.startsWith(":")) line.trim.drop(1) match {
+    val trimmedLine = line.trim
+    if (!trimmedLine.isEmpty) {
+      if (line.trim.startsWith(":")) trimmedLine.drop(1) match {
         case "quit" | "q" => sys.exit(0)
         case "synt" | "s" => {
           out.println(s"${infoColor}Synthesizing... (Press any key to interrupt)${Console.RESET}")
@@ -74,48 +111,26 @@ object ShellMain extends App {
           if (results.isEmpty) {
             out.println(s"${infoColor}No results, try waiting a bit longer${Console.RESET}")
           } else {
-            val resultStrings = results.map(_.program.code)
+            currentResults = results.map(_.program.code).toList
             val expectedResults = task.examples.map(_.output)
             val fits = results.map(_.program.values.zip(expectedResults).count(pair => pair._1 == pair._2))
             val n = expectedResults.length
-            resultStrings.reverse.foreach(reader.getHistory.add(_))
-            resultStrings.zip(fits).zipWithIndex.foreach(
+            currentResults.reverse.foreach(reader.getHistory.add(_))
+            currentResults.zip(fits).zipWithIndex.foreach(
               { case ((p, fit), i) => out.println(s"$infoColor${i + 1}:${Console.RESET} $p $infoColor[$fit/$n]${Console.RESET}") })
           }
         }
-        case _ => out.println("Not a valid command, try :quit or :synt")
-      } else {
-      val lexer = new SyGuSLexer(CharStreams.fromString(line))
-      lexer.removeErrorListeners()
-      lexer.addErrorListener(new ThrowingLexerErrorListener)
-      val parser = new SyGuSParser(new BufferedTokenStream(lexer))
-      parser.removeErrorListeners()
-      parser.setErrorHandler(new BailErrorStrategy)
-      val parsed = parser.bfTerm()
-      if (parser.getCurrentToken.getType != Token.EOF)
-        throw new EOFExpectedException(parser)
-      val visitor = new ASTGenerator(task)
-      val ast = visitor.visit(parsed)
-      out.println(Tabulator.format(List("input","result","expected") +:
-        task.examples.zip(ast.values).map(pair => List(pair._1.input.toList.map(kv =>
-          s"${kv._1} -> ${escapeIfString(kv._2.toString)}"
-        ).mkString("\n"),
-          if (pair._2 == pair._1.output) GreenString(escapeIfString(pair._2)) else escapeIfString(pair._2),
-          escapeIfString(pair._1.output)))))
-    }} catch {
-      case e: RecognitionException => {
-        prettyPrintSyntaxError(e)
-      }
-      case e: ResolutionException => {
-        val startIdx = e.badCtx.getStart.getStartIndex
-        val len = e.badCtx.getStop.getStopIndex - startIdx + 1
-        out.println(errorColor + " " * (startIdx + 2) + "^" + (if (len > 1) "-" * (len - 2) + "^" else ""))
-        out.println("Cannot resolve program" + Console.RESET)
-      }
-      case e: ParseCancellationException =>{
-        prettyPrintSyntaxError(e.getCause.asInstanceOf[RecognitionException])
-      }
+        case s => allCatch opt s.toInt match {
+          case None => out.println(s"${infoColor}Not a valid command, try :quit or :synt${Console.RESET}")
+          case Some(idx) => currentResults.lift(idx - 1) match {
+            case None => out.println(s"${infoColor}Try index between 1 and ${currentResults.length}${Console.RESET}")
+            case Some(p) => {
+              out.println(p)
+              evalExpr(p)
+            }
+          }
+        }
+      } else evalExpr(trimmedLine)
     }
   }
-
 }
