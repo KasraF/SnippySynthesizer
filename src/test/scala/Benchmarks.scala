cbebed7b64007b69102c5188279ba6275a31082f
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2020-03-11 22:27:13 -0700

    modified benchmarks to run all at ones

--- src/test/scala/Benchmarks.scala
+++ src/test/scala/Benchmarks.scala
@@ -7,38 +7,41 @@ import scala.concurrent.duration.FiniteDuration
 
 object Benchmarks extends App
 {
-	val benchmarks = new File("src/test/resources/benchmarks")
-	assert(benchmarks.isDirectory)
-
 	println(
 		"       _____            ______      \n" +
-		"      /  ___|           | ___ \\     \n" +
-		"      \\ `--. _   _ _ __ | |_/ /   _ \n" +
-		"       `--. \\ | | | '_ \\|  __/ | | |\n" +
-		"      /\\__/ / |_| | | | | |  | |_| |\n" +
-		"      \\____/ \\__, |_| |_\\_|   \\__, |\n" +
-		"              __/ |            __/ |\n" +
-		"             |___/            |___/ \n" +
-		"-------------------------------------------\n" +
-		"| Python Synthesizer for Projection Boxes |\n" +
-		"-------------------------------------------\n")
-
+		  "      /  ___|           | ___ \\     \n" +
+		  "      \\ `--. _   _ _ __ | |_/ /   _ \n" +
+		  "       `--. \\ | | | '_ \\|  __/ | | |\n" +
+		  "      /\\__/ / |_| | | | | |  | |_| |\n" +
+		  "      \\____/ \\__, |_| |_\\_|   \\__, |\n" +
+		  "              __/ |            __/ |\n" +
+		  "             |___/            |___/ \n" +
+		  "-------------------------------------------\n" +
+		  "| Python Synthesizer for Projection Boxes |\n" +
+		  "-------------------------------------------\n")
 	println("Index Name                 Program")
-	println("----- -------------------- --------------------------------------")
-	benchmarks.listFiles()
-	  .map(_.getAbsolutePath)
-	  .filter(_.contains(".examples.json"))
-	  .filter(!_.contains(".out"))
-	  .zipWithIndex
-	  .sorted
-	  .foreach(benchmark => {
-		  val file = benchmark._1
-		  val index = benchmark._2 + 1
-		  val name: String = file.substring(file.lastIndexOf("/") + 1, file.indexOf("."))
-		  print(f"($index%2d)  [$name%18s] ")
-		  Main.synthesize(file) match {
-			  case None => println("Timeout")
-			  case Some((program: String, time: Int)) => println(f"[${time / 1000.0}%1.3f] $program")
-		  }
-	  })
+
+	val benchmarks = new File("src/test/resources")
+	assert(benchmarks.isDirectory)
+
+	benchmarks.listFiles().filter(_.isDirectory).foreach(
+		dir => {
+			println("----- -------------------- --------------------------------------")
+			dir.listFiles()
+			  .map(_.getAbsolutePath)
+			  .filter(_.contains(".examples.json"))
+			  .filter(!_.contains(".out"))
+			  .sorted
+			  .zipWithIndex
+			  .foreach(benchmark => {
+				  val file = benchmark._1
+				  val index = benchmark._2 + 1
+				  val name: String = file.substring(file.lastIndexOf("/") + 1, file.indexOf("."))
+				  print(f"($index%2d)  [$name%18s] ")
+				  Main.synthesize(file) match {
+					  case None => println("Timeout")
+					  case Some((program: String, time: Int)) => println(f"[${time / 1000.0}%1.3f] $program")
+				  }
+			  })
+		})
 }
