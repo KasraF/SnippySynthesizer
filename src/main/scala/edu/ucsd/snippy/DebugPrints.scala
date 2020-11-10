6b41a43fd79f09bea0072a75d33d922ae885032d
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2020-11-09 16:22:39 -0800

    use slf4j for logging in the synth

--- src/main/scala/edu/ucsd/snippy/DebugPrints.scala
+++ src/main/scala/edu/ucsd/snippy/DebugPrints.scala
@@ -1,21 +1,18 @@
 package edu.ucsd.snippy
 
-object DebugPrints {
-  var debug = true
-  var info = false
-  def setDebug() = {
-    debug = true
-    info = true
-  }
-  def setInfo() = {
-    debug = false
-    info = true
-  }
-  def setNone() = {
-    debug = false
-    info = false
-  }
-  def dprintln(str: => Any) = if (debug) println(str)
-  def iprintln(str: => Any) = if (info) println(str)
-  def eprintln(str: => Any) = System.err.println(str)
+import org.slf4j.{Logger, LoggerFactory}
+
+object DebugPrints
+{
+	val logger: Logger = LoggerFactory.getLogger(this.getClass)
+	val debug: Boolean = this.logger.isDebugEnabled
+	val info: Boolean = this.logger.isInfoEnabled
+
+	def dprintln(msg: String, args: Any*) = this.logger.debug(msg, args: _*)
+
+	def iprintln(msg: String, args: Any*) = this.logger.info(msg, args: _*)
+
+	def eprintln(msg: String, args: Any*) = this.logger.error(msg, args: _*)
+
+	def eprintln(msg: String, error: Throwable) = this.logger.error(msg, error: Throwable)
 }
