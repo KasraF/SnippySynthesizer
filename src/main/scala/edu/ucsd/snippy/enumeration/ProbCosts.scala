e4dfde8da687350ce33f5473cdd2fab87079e7e0
Shraddha Barke <shraddha.6596@gmail.com>
2021-02-25 14:52:58 +0530

    made getCosts more general

--- src/main/scala/edu/ucsd/snippy/enumeration/ProbCosts.scala
+++ src/main/scala/edu/ucsd/snippy/enumeration/ProbCosts.scala
@@ -5,10 +5,16 @@ object ProbCosts
 	def getCosts(childrenCost: Double, childrenCosts: Array[Int], childrenArity: Int): Array[Array[Int]] =
 	{
 		var candidateCosts = Array[Array[Int]]()
-
-		val combinations = (childrenCosts ++ childrenCosts ++ childrenCosts).combinations(childrenArity).filter(c => c.sum == childrenCost)
+		val combinations = repeatElementsInList(childrenCosts, childrenArity)
+			.combinations(childrenArity).filter(c => c.sum == childrenCost)
 		candidateCosts = combinations.flatMap(c => c.permutations).toArray
 		candidateCosts = candidateCosts.distinct
 		candidateCosts
+	}
+
+	 def repeatElementsInList(list: Array[Int], times: Int): Array[Int] = {
+		list.flatMap (x =>
+			List.fill(times)(x)
+		)
 	}
 }
