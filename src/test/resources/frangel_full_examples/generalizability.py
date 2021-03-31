8e58193dd4429d22ee1fd0e5e27f70d654a9fb9a
peleghila <peleg.hila@gmail.com>
2021-03-31 03:57:50 -0700

    it's nicer when code compiles

--- src/test/resources/frangel_full_examples/generalizability.py
+++ src/test/resources/frangel_full_examples/generalizability.py
@@ -2,7 +2,7 @@ import re
 import json
 
 def do_program(bench,prog):
-    with open(bench + '.allex.json") as json_file:
+    with open(bench + '.allex.json') as json_file:
         data = json.load(json_file)
     full_prog = data['loopheader'] + '\n' + '\n'.join(['  ' + l for l in curr_program.split('\n')])
     success = 0
@@ -19,20 +19,20 @@ def test_example(example,full_prog):
             return False
     return True
 
-results_file = None
-with open(results_file) as f:
-    lines = f.readlines()
-curr_program = ''
-tab = 0
-bench_name = ''
-for l in lines:
-	m = re.match(r'^\(.*?\) \[.\] \[(.*?)\] \[.*?\] \[.*?\] (.*)$',l)
-	if m:
-		if curr_program: do_program(bench_name,curr_program)
-		curr_program = m[2]
-		bench_name = m[1].strip()
-		tab = len(m[0]) - len(m[2])
-	elif l.startswith(' ') and curr_program:
-		curr_program += '\n' + l[tab:].rstrip()
-if curr_program: do_program(bench_name,curr_program)
+def do_compares(results_file):
+    with open(results_file) as f:
+        lines = f.readlines()
+    curr_program = ''
+    tab = 0
+    bench_name = ''
+    for l in lines:
+        m = re.match(r'^\(.*?\) \[.\] \[(.*?)\] \[.*?\] \[.*?\] (.*)$',l)
+        if m:
+            if curr_program: do_program(bench_name,curr_program)
+            curr_program = m[2]
+            bench_name = m[1].strip()
+            tab = len(m[0]) - len(m[2])
+        elif l.startswith(' ') and curr_program:
+            curr_program += '\n' + l[tab:].rstrip()
+    if curr_program: do_program(bench_name,curr_program)
 
