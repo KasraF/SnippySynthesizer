2b911ae2170c10be9443a2190c02fb6ff36073d9
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2021-07-11 15:04:19 -0700

    added programming tasks

---
+++ programming_tasks/1_string_compression.py
@@ -0,0 +1,29 @@
+"""
+String Compression:
+	Implement a method for basic string
+	compression, using the count of
+	repeated characters. You can assume
+	that the string is non-empty, and 
+	only contains alphabetic characters.
+	>>> task('aabccca')
+	'2a1b3c1a'
+"""
+def task(s):
+	rs = ''
+	
+	return rs
+
+task('aabccca')
+
+"""
++----------------+
+|  Cheat Sheet:  |
++----------------+
+variable = ??
+
+Tab           -> Go to next variable/row
+Shift + Tab   -> Go to previous variable/row
+Shift + Enter -> Add example, go to next item 
+Enter         -> Submit examples to LooPy
+Esc           -> Exit LooPy
+"""
---
+++ programming_tasks/3_extract_numbers.py
@@ -0,0 +1,28 @@
+"""
+Extract Numbers:
+	Given a string containing numbers
+	separated by exactly one non-integer
+	character, return a list containing
+	all the numbers in the string.
+	>>> task('13a7b42')
+	[13, 7, 42]
+"""
+def task(s):
+	rs = []
+	
+	return rs
+
+task('13a7b42')
+
+"""
++----------------+
+|  Cheat Sheet:  |
++----------------+
+variable = ??
+
+Tab           -> Go to next variable/row
+Shift + Tab   -> Go to previous variable/row
+Shift + Enter -> Add example, go to next item 
+Enter         -> Submit examples to LooPy
+Esc           -> Exit LooPy
+"""
