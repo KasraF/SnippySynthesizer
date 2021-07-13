7bd7736909394fe59ab3dec18dc40d82c6c4f36e
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2021-07-12 17:25:26 -0700

    added benchmark and build scripts

---
+++ scripts/figure11.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo -n "Compiling... ";
+if BUILD_RESULTS=$(mvn clean compile -DskipTests); then
+    echo "OK";
+    mvn exec:java -Dexec.mainClass="edu.ucsd.snippy.SimBenchmarksCSV";
+else
+    echo "Compiling the synthesizer failed:";
+    echo "$BUILD_RESULTS";
+fi;
